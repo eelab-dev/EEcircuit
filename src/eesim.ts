9c6cc53116dee543fcf7251f83ff8be4b8c6ac23
danchitnis <danchitnis@users.noreply.github.com>
2020-10-24 05:45:13 +0100

    added contrast color

--- src/eesim.ts
+++ src/eesim.ts
@@ -7,6 +7,7 @@ import Simulation, { ResultType } from "./simulation";
 import { CodeJar } from "codejar";
 import { printCSV, printDisplay } from "./printOutput";
 import WebglPlot, { ColorRGBA, WebglLine } from "webgl-plot";
+import { calcContrast, calcLuminance } from "./calcContrast";
 
 const resultArea = document.getElementById("textArea");
 
@@ -35,6 +36,19 @@ btSim.addEventListener("click", () => {
   sim.runSim();
 });
 
+const getColor = (): ColorRGBA => {
+  let contrast = 0;
+  let r, g, b;
+  while (contrast < 7) {
+    r = Math.random();
+    g = Math.random();
+    b = Math.random();
+
+    contrast = calcContrast(calcLuminance(b, g, r), calcLuminance(0.2, 0.2, 0.2));
+  }
+  return new ColorRGBA(r, g, b, 1);
+};
+
 const btPlot = document.getElementById("btPlot") as HTMLButtonElement;
 btPlot.addEventListener("click", () => {
   sim.setNetList(jar.toString());
@@ -53,13 +67,13 @@ btPlot.addEventListener("click", () => {
     wglp.gOffsetX = -1;
     wglp.gScaleX = 2;
 
-    const color = new ColorRGBA(1, 1, 0, 1);
     const data = results.data;
 
     let maxY = 0;
     let minY = 0;
 
     for (let col = 1; col < data.length; col++) {
+      const color = getColor();
       const line = new WebglLine(color, data[0].length);
       const maxX = data[0][data[0].length - 1];
 
