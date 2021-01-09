266e25be9997c472b8dedb3951fe477c271a7aa7
danchitnis <danchitnis@users.noreply.github.com>
2021-01-09 23:34:10 +0000

    midpoint

--- src/axis.tsx
+++ src/axis.tsx
@@ -1,31 +1,54 @@
 import type { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import React, { useState, useEffect, useRef } from "react";
 
-const Axis = (): JSX.Element => {
+type AxisType = {
+  scale: number;
+  offset: number;
+};
+
+type CanvasSize = {
+  width: number;
+  height: number;
+};
+
+let ctx: CanvasRenderingContext2D | null;
+
+const Axis = ({ scale, offset }: AxisType): JSX.Element => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasSize, setCanvasSize] = useState<CanvasSize>({ width: 0, height: 0 });
+
+  console.log("axis->", scale, ", ", offset);
+  //console.log("axis->", midpoint);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const devicePixelRatio = window.devicePixelRatio || 1;
+
     if (canvas) {
       const devicePixelRatio = window.devicePixelRatio || 1;
       canvas.width = canvas.clientWidth * devicePixelRatio;
       canvas.height = canvas.clientHeight * devicePixelRatio;
 
-      const ctx = canvas?.getContext("2d");
+      setCanvasSize({ width: canvas.width, height: canvas.height });
+
+      ctx = canvas?.getContext("2d");
 
       const rect = canvas?.getBoundingClientRect();
       console.log("axis->", rect);
-      if (ctx) {
-        ctx.font = "16px serif";
-        ctx.fillText("1234", 60, 15);
-        //ctx.fillRect(10, 10, 100, 100);
-        ctx.moveTo(60, 0);
-        ctx.lineTo(60, 10);
-        ctx.stroke();
-      }
     }
   }, [canvasRef]);
+
+  useEffect(() => {
+    if (ctx) {
+      const midpoint = -offset / scale;
+      ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
+      ctx.font = "16px serif";
+      ctx.fillText(midpoint.toExponential(2), canvasSize.width / 2, 15);
+      //ctx.fillRect(10, 10, 100, 100);
+      ctx.moveTo(canvasSize.width / 2, 0);
+      ctx.lineTo(canvasSize.width / 2, 10);
+      ctx.stroke();
+    }
+  }, [scale, offset]);
 
   return <canvas style={{ height: "1.5em", width: "100%" }} ref={canvasRef} />;
 };
