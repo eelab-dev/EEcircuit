8b058e1db2e109fdd7f0dc0270c87cdbc5a19bdf
danchitnis <danchitnis@users.noreply.github.com>
2023-08-27 21:48:55 +0100

    yarn update

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,3 @@ const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<Layout />);
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot?.accept(({ module }) => {});
-}
