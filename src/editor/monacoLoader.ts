209184759a3bd81d4d899643fdada1f5841c4bcd
danchitnis <danchitnis@users.noreply.github.com>
2024-08-30 02:02:54 +0000

    Auto updates after tests

--- src/editor/monacoLoader.ts
+++ src/editor/monacoLoader.ts
@@ -7,7 +7,7 @@ import type * as MonacoEditor from "monaco-editor/esm/vs/editor/editor.api";
 
 const monacoLoader = (): Promise<typeof MonacoEditor> => {
   return new Promise((resolve) => {
-    const monacoPath = "https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs";
+    const monacoPath = "https://cdn.jsdelivr.net/npm/monaco-editor@0.51.0/min/vs";
 
     const loaded2 = (e: CustomEvent) => {
       resolve(e.detail);
