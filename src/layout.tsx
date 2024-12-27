5536ce031f56cfc98a968b1448d6f02ad8e5c5a6
Danial Chitnis <danchitnis@users.noreply.github.com>
2024-12-27 22:55:01 +0000

    analytics

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,8 @@
 import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 import React, { JSX } from "react";
 import EEcircuit from "./EEcircuit";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const Layout = (): JSX.Element => {
   const config: ThemeConfig = {
@@ -16,6 +17,8 @@ const Layout = (): JSX.Element => {
       <ChakraProvider theme={customTheme}>
         <EEcircuit />
       </ChakraProvider>
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 };
