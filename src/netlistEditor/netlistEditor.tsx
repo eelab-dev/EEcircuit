5494a3ff0e31777f1aa69258e04becb49d4d16b8
Danial Chitnis <danchitnis@users.noreply.github.com>
2025-06-10 23:18:21 +0100

    better layout

--- src/netlistEditor/netlistEditor.tsx
+++ src/netlistEditor/netlistEditor.tsx
@@ -47,14 +47,11 @@ const NetlistEditor: React.FC<NetlistEditorProps> = ({
       globalThis.removeEventListener("resize", handleResize);
     };
   }, []);
-  const handleEditor = React.useCallback(
-    (value: string | undefined) => {
-      if (value !== undefined) {
-        setNetListToSim(value);
-      }
-    },
-    []
-  );
+  const handleEditor = React.useCallback((value: string | undefined) => {
+    if (value !== undefined) {
+      setNetListToSim(value);
+    }
+  }, []);
 
   useEffect(() => {
     if (selectedSimType === "None") {
@@ -96,63 +93,140 @@ const NetlistEditor: React.FC<NetlistEditorProps> = ({
   };
 
   return (
-    <Flex width="100%" flexDirection={"column"} gap={4}>
-      <Suspense fallback={<Skeleton height="40vh" width="100%" />}>
-        <EditorCustom
-          height="50vh"
-          width="100%"
-          language="spice"
-          value={netListToSim}
-          valueChanged={handleEditor}
-          theme={useColorModeValue("light", "dark")}
-          key={windowSize.width}
-        />
-      </Suspense>
-      <Flex flexDirection="row" gap={4} width="100%">
-        <RadioCard.Root
-          defaultValue={simType[0]}
-          value={selectedSimType}
-          gap="4"
-          maxW="sm"
-          onValueChange={(value: RadioCardValueChangeDetails) => {
-            if (value.value) {
-              setSelectedSimType(value.value);
-            }
+    <Flex
+      width="100%"
+      flexDirection={"row"}
+      height="100%"
+      overflow="hidden"
+      position="relative"
+      css={{
+        "& *": {
+          scrollbarWidth: "thin",
+        },
+        "&, & *": {
+          overscrollBehavior: "contain",
+        },
+      }}
+    >
+      {/* Editor on the left */}
+      <Flex flex="1" flexDirection="column" height="100%" overflow="hidden">
+        <Suspense fallback={<Skeleton height="100%" width="100%" />}>
+          <EditorCustom
+            height="100%"
+            width="100%"
+            language="spice"
+            value={netListToSim}
+            valueChanged={handleEditor}
+            theme={useColorModeValue("light", "dark")}
+            key={windowSize.width}
+          />
+        </Suspense>
+      </Flex>
+
+      {/* Simulation options on the right */}
+      <Flex
+        flexDirection="column"
+        minWidth="300px"
+        maxWidth="400px"
+        height="100%"
+        overflow="hidden"
+        borderLeft="1px solid"
+        borderColor={useColorModeValue("gray.200", "gray.600")}
+        position="relative"
+      >
+        {/* Scrollable config area */}
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: "80px",
+            overflowY: "auto",
+            overflowX: "hidden",
+            padding: "16px",
+            overscrollBehavior: "contain",
+            scrollbarWidth: "thin",
+            scrollbarColor: useColorModeValue(
+              "rgb(203, 213, 225) transparent",
+              "rgb(75, 85, 99) transparent"
+            ),
+          }}
+          onWheel={(e) => {
+            e.stopPropagation();
+          }}
+          onTouchMove={(e) => {
+            e.stopPropagation();
           }}
         >
-          <RadioCard.Label>Simulation type</RadioCard.Label>
-          <Group attached orientation="vertical">
-            {simType.map((type) => (
-              <RadioCard.Item key={type} value={type} width="full">
-                <RadioCard.ItemHiddenInput />
-                <RadioCard.ItemControl>
-                  <RadioCard.ItemIndicator />
-                  <RadioCard.ItemContent>
-                    <RadioCard.ItemText>{type}</RadioCard.ItemText>
-                  </RadioCard.ItemContent>
-                </RadioCard.ItemControl>
-              </RadioCard.Item>
-            ))}
-          </Group>
-        </RadioCard.Root>
-        {(() => {
-          switch (selectedSimType) {
-            case "None":
-              return (
-                <>
-                  <p>No addition to the netlist</p>
-                </>
-              );
-            case "DC":
-              return <DcConfig onConfigChange={handleConfigChange} />;
-            case "AC":
-              return <AcConfig onConfigChange={handleConfigChange} />;
-            case "Trans":
-              return <TransConfig onConfigChange={handleConfigChange} />;
-          }
-        })()}
+          <Flex flexDirection="column" gap={4}>
+            <RadioCard.Root
+              defaultValue={simType[0]}
+              value={selectedSimType}
+              gap="4"
+              onValueChange={(value: RadioCardValueChangeDetails) => {
+                if (value.value) {
+                  setSelectedSimType(value.value);
+                }
+              }}
+            >
+              <RadioCard.Label>Simulation type</RadioCard.Label>
+              <Group
+                attached
+                display="grid"
+                gridTemplateColumns="repeat(2, 1fr)"
+                gap="0"
+              >
+                {simType.map((type) => (
+                  <RadioCard.Item key={type} value={type} width="full">
+                    <RadioCard.ItemHiddenInput />
+                    <RadioCard.ItemControl>
+                      <RadioCard.ItemIndicator />
+                      <RadioCard.ItemContent>
+                        <RadioCard.ItemText>{type}</RadioCard.ItemText>
+                      </RadioCard.ItemContent>
+                    </RadioCard.ItemControl>
+                  </RadioCard.Item>
+                ))}
+              </Group>
+            </RadioCard.Root>
+            {(() => {
+              switch (selectedSimType) {
+                case "None":
+                  return (
+                    <>
+                      <p>No addition to the netlist</p>
+                    </>
+                  );
+                case "DC":
+                  return <DcConfig onConfigChange={handleConfigChange} />;
+                case "AC":
+                  return <AcConfig onConfigChange={handleConfigChange} />;
+                case "Trans":
+                  return <TransConfig onConfigChange={handleConfigChange} />;
+              }
+            })()}
+          </Flex>
+        </div>
+
+        {/* Fixed button at bottom */}
+        <Flex
+          position="absolute"
+          bottom="0"
+          left="0"
+          right="0"
+          height="80px"
+          padding="4"
+          backgroundColor={useColorModeValue("white", "gray.800")}
+          borderTop="1px solid"
+          borderColor={useColorModeValue("gray.200", "gray.600")}
+          alignItems="center"
+        >
+          <Button onClick={handleSimRun} width="100%">
+            Run Simulation
+          </Button>
+        </Flex>
       </Flex>
-      <Button onClick={handleSimRun}>Run Simulation</Button>
     </Flex>
   );
 };
