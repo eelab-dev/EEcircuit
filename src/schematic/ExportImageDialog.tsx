1ecd609e35f44b56b505b45a9abcad82d265c26e
Danial Chitnis <danchitnis@users.noreply.github.com>
2025-08-17 03:57:42 +0100

    Fix PDF download by wrapping pdfBytes in Uint8Array
    Wrapped pdfBytes in a Uint8Array when creating the Blob to ensure correct PDF file generation and download.

--- src/schematic/ExportImageDialog.tsx
+++ src/schematic/ExportImageDialog.tsx
@@ -114,7 +114,9 @@ const ExportImageDialog: React.FC<ExportImageDialogProps> = ({
       });
 
       // Download the PDF
-      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+      const blob = new Blob([new Uint8Array(pdfBytes)], {
+        type: "application/pdf",
+      });
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
