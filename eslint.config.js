008cea749da5d3377686753dcc5b6554d928b61c
Danial Chitnis <danchitnis@users.noreply.github.com>
2025-09-06 01:49:13 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default defineConfig([
 
   // Base JS config for src
   {
-    files: ["src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ["src/**/*.{ts,mts,cts,jsx,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: {
