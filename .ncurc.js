942a133272fdef6209dd5520ad725a0baa412738
Danial Chitnis <danchitnis@users.noreply.github.com>
2026-02-15 16:31:22 +0000

    update chore & eslint update bug

---
+++ .ncurc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // https://github.com/eslint-config/airbnb-extended/issues/65
+  reject: [
+    'eslint',
+    '@eslint/js',
+    'typescript-eslint',
+    '@types/eslint'
+  ],
+};
